685dab5827164cda00398f557fa0d7f37fb706a2
thanhtung1010 <trinhthanhtung1010@gmail.com>
2023-11-02 22:38:02 +0700

    add missing env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -6,4 +6,12 @@ export const enviroment = {
   email: 'trinhthanhtung1010@gmail.com',
   phoneNumber: '+84836450670',
   defaultPageSize: 10,
+  SETTING_FORMAT: {
+    dateTime: {
+      date: "DD/MM/YYYY",
+      time: "HH:mm:ss a",
+      dateTime: "DD/MM/YYYY HH:mm:ss",
+      portfolioDate: "MMM - yyyy"
+    },
+  },
 }
